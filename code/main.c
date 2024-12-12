ffc84c8b422699513f4e64fe170635ee646b5e35
Max Inden <mail@max-inden.de>
2024-12-12 15:44:23 +0100

    Adjust code/main.c

--- code/main.c
+++ code/main.c
@@ -22,10 +22,12 @@
 
 #ifdef __linux__
     typedef int sock_optval_t;
-    #define IPV4_DONTFRAG_OPTNAME IP_MTU_DISCOVER
-    #define IPV4_DONTFRAG_OPTVAL IP_PMTUDISC_DO
-    #define IPV6_DONTFRAG_OPTNAME IPV6_MTU_DISCOVER
-    #define IPV6_DONTFRAG_OPTVAL IPV6_PMTUDISC_DO
+    #define IPV4_MTU_DISCOVER_OPTNAME IP_MTU_DISCOVER
+    #define IPV4_MTU_DISCOVER_OPTVAL IP_PMTUDISC_PROBE
+    #define IPV6_MTU_DISCOVER_OPTNAME IPV6_MTU_DISCOVER
+    #define IPV6_MTU_DISCOVER_OPTVAL IPV6_PMTUDISC_PROBE
+    #define IPV6_DONTFRAG_OPTNAME IPV6_DONTFRAG
+    #define IPV6_DONTFRAG_OPTVAL 1
 #elif defined(__APPLE__)
     typedef int sock_optval_t;
     #define IPV4_DONTFRAG_OPTNAME IP_DONTFRAG
@@ -147,12 +149,21 @@ int main(int argc, char *argv[]) {
     #endif
 
     if(should_set_ipv4_df) {
+        #ifdef __linux__
+        sock_optval_t opt_val = IPV4_MTU_DISCOVER_OPTVAL;
+        if (setsockopt(sock, IPPROTO_IP, IPV4_MTU_DISCOVER_OPTNAME, &opt_val, sizeof(opt_val)) < 0) {
+            perror("setsockopt ipv4 mtu discover");
+            close(sock);
+            return 1;
+        }
+        #else
         sock_optval_t opt_val = IPV4_DONTFRAG_OPTVAL;
         if (setsockopt(sock, IPPROTO_IP, IPV4_DONTFRAG_OPTNAME, &opt_val, sizeof(opt_val)) < 0) {
             perror("setsockopt ipv4 dontfrag");
             close(sock);
             return 1;
         }
+        #endif
     }
 
     if(net_type == IPV6 || net_type == DUAL) {
@@ -162,6 +173,14 @@ int main(int argc, char *argv[]) {
             close(sock);
             return 1;
         }
+        #ifdef __linux__
+        opt_val = IPV6_MTU_DISCOVER_OPTVAL;
+        if (setsockopt(sock, IPPROTO_IP, IPV6_MTU_DISCOVER_OPTNAME, &opt_val, sizeof(opt_val)) < 0) {
+            perror("setsockopt ipv6 mtu discover");
+            close(sock);
+            return 1;
+        }
+        #endif
     }
 
     for (int i = 0; i < PACKET_COUNT; i++) {
